6402cabaa8374b34eae950fe05822c3fc78e8bca
artemkot <artem.alex.kot@gmail.com>
2024-09-24 21:34:11 +0200

    Updated location of the pdf generator file

---
+++ src/main/java/org/invoicegen/pdf/PdfGenerator.java
@@ -0,0 +1,7 @@
+package org.invoicegen.pdf;
+
+public class PdfGenerator {
+
+//    Sample code below
+//    Google sheet with invoice data: https://docs.google.com/spreadsheets/d/11_ktHshaxh-xnxCzdIMJ1iWyQTXfguXNy2RdsYTBwJA/edit?gid=1986086745#gid=1986086745
+}
